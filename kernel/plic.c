05ee8d5bc2d78a66b7cae898518f6db53e15bc5f
Peter <peter@quantr.hk>
2022-12-01 16:50:36 +0800

    dev

--- kernel/plic.c
+++ kernel/plic.c
@@ -35,6 +35,7 @@ plic_claim(void)
 {
   int hart = cpuid();
   int irq = *(uint32*)PLIC_SCLAIM(hart);
+  printf("plic_claim %x\n", irq);
   return irq;
 }
 
@@ -43,5 +44,6 @@ void
 plic_complete(int irq)
 {
   int hart = cpuid();
+  printf("plic_complete %x\n", irq);
   *(uint32*)PLIC_SCLAIM(hart) = irq;
 }
